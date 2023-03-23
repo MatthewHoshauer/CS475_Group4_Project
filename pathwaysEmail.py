2246d87a4f8b4ee271d538340bcb308968860ae2
Matthew Hoshauer <hoshauerm2@mailbox.winthrop.edu>
2023-03-23 14:56:43 -0400

    Mock Pull from .XLSX. file

--- pathwaysEmail.py
+++ pathwaysEmail.py
@@ -2,7 +2,15 @@ from socket import *
 
 import openpyxl
 
-dataframe = openpyxl.load_workbook()
+dataframe = openpyxl.load_workbook("TestBook.xlsx")
+
+# Define variable to read sheet
+dataframe1 = dataframe.active
+ 
+# Iterate the loop to read the cell values
+for row in range(0, dataframe1.max_row):
+    for col in dataframe1.iter_cols(1, dataframe1.max_column):
+        print(col[row].value)
 
 
 
