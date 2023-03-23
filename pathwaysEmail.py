d47d629f4438b0f77dc0635447e41ad9a2c8385a
Matthew Hoshauer <hoshauerm2@mailbox.winthrop.edu>
2023-03-23 15:22:54 -0400

    basic read in functionality

--- pathwaysEmail.py
+++ pathwaysEmail.py
@@ -1,16 +1,30 @@
 from socket import *
 
-import openpyxl
+# import openpyxl
 
-dataframe = openpyxl.load_workbook("TestBook.xlsx")
+# dataframe = openpyxl.load_workbook("TestBook.csv")
 
-# Define variable to read sheet
-dataframe1 = dataframe.active
+# # Define variable to read sheet
+# dataframe1 = dataframe.active
  
-# Iterate the loop to read the cell values
-for row in range(0, dataframe1.max_row):
-    for col in dataframe1.iter_cols(1, dataframe1.max_column):
-        print(col[row].value)
+# # Iterate the loop to read the cell values
+# for row in range(0, dataframe1.max_row):
+#     for col in dataframe1.iter_cols(1, dataframe1.max_column):
+#         print(col[row].value)
+
+# import csv
+
+# file = open('TestBook.csv')
+
+# type(file)
+
+import pandas as pd
+
+sample_data = pd.read_csv('TestBook.csv')
+
+print(sample_data.head)
+
+print("test===================")
 
 
 
