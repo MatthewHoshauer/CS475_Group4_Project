5cdc166f3d61406557d182feed5a33e0fda520a8
owense6 <owense6@mailbox.winhtrop.edu>
2023-03-29 21:03:51 -0400

    it works

--- pathwaysEmail.py
+++ pathwaysEmail.py
@@ -15,10 +15,6 @@ needs = df["needs"].iloc[-1]
 clientNum = df["client phone number"].iloc[-1]
 agencyEmail = df["agency email"].iloc[-1]
 
-print('\n\n')
-print("test===================")
-
-
 if clientNum != None:
     msg = "\r\nHello " + agencyName + ", you have an upcoming appointment with " + clientName + " on " + apptDate + ". Their needs are " + needs + ".\n"
     msg = msg + "The client's phone number is " + clientNum + " if more information is needed from the client.\n\n--Pathways Staff\nPathways Phone: (803) 366-PATH (7284)"
@@ -36,45 +32,33 @@ mailserver = ("list.winthrop.edu", 25)
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect(mailserver)
 recv = clientSocket.recv(1024).decode()
-print(recv)
-if recv[:3] != '220':
-    print('220 reply not received from server.')
 
 # Send HELO command and print server response.
 heloCommand = 'HELO list.winthrop.edu\r\n'
 clientSocket.send(heloCommand.encode())
 recv1 = clientSocket.recv(1024).decode()
-print(recv1)
-if recv1[:3] != '250':
-    print('250 reply not received from server.')
 heloCommand = 'HELO list.winthrop.edu\r\n'
 clientSocket.send(heloCommand.encode())
 recv1 = clientSocket.recv(1024)
 recv1 = recv1.decode()
-print("Message after HELO command:" + recv1)
-if recv1[:3] != '250':
-    print('250 reply not received from server.')
 
 # Send MAIL FROM command and print server response.
 mailFrom = "MAIL FROM: pathwaystest@gmail.com\r\n"
 clientSocket.send(mailFrom.encode())
 recv2 = clientSocket.recv(1024)
 recv2 = recv2.decode()
-print("After MAIL FROM command: " + recv2)
 
 # Send RCPT TO command and print server response.
 rcptTo = "RCPT TO: owense6@mailbox.winthrop.edu\r\n"
 clientSocket.send(rcptTo.encode())
 recv3 = clientSocket.recv(1024)
 recv3 = recv3.decode()
-print("After RCPT TO command: " + recv3)
 
 # Send DATA command and print server response.
 data = "DATA\r\n"
 clientSocket.send(data.encode())
 recv4 = clientSocket.recv(1024)
 recv4 = recv4.decode()
-print("After DATA command: " + recv4)
 
 # Send message data.
 subject = "Subject: Upcoming Client Appointment.\r\n\r\n" 
@@ -82,16 +66,13 @@ clientSocket.send(subject.encode())
 clientSocket.send(msg.encode())
 clientSocket.send(endmsg.encode())
 recv_msg = clientSocket.recv(1024)
-print("Response after sending message body:" + recv_msg.decode())
 
 # Message ends with a single period.
 clientSocket.send(endmsg.encode())
 recv_endmsg = clientSocket.recv(1024)
-print("Response after sending the single period:" + recv_endmsg.decode())
 
 # Send QUIT command and get server response.
 quit = "QUIT\r\n"
 clientSocket.send(quit.encode())
 recv5 = clientSocket.recv(1024)
-print(recv5.decode())
 clientSocket.close()
