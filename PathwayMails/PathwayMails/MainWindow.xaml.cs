41ad93c9d66eea2df119fddaafd7ff51020f4bc2
Matthew Hoshauer <hoshauerm2@mailbox.winthrop.edu>
2023-04-10 21:09:09 -0400

    Uhmmmm we're going a little stupid...

---
+++ PathwayMails/PathwayMails/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using IronPython.Hosting;
+using IronPython.Runtime;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
+using IronPython;
+
+
+
+
+namespace PathwayMails
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button is clicked!");
+
+            
+
+            ScriptEngine engine = Python.CreateEngine();
+            ScriptScope scope = engine.CreateScope();
+            engine.ExecuteFile("pathwaysEmail.py", scope);
+
+            MessageBox.Show("ITS FINISHED");
+
+        }
+    }
+}
--- PathwaysMailer/PathwaysMailer/MainWindow.xaml.cs
+++ PathwaysMailer/PathwaysMailer/MainWindow.xaml.cs
@@ -12,17 +12,19 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using IronPython.Hosting;
 
 namespace PathwaysMailer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+   
+
+       
     }
 }
